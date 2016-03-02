e816f8a9165cae04e70ae05ab1aa1bdc9999b974
Imer D <xidc_xidc@msn.com>
2016-03-02 17:24:41 +0000

    Initial commit add bootrap , create CRUD company

---
+++ company_admin/app/controllers/companies_controller.rb
@@ -0,0 +1,52 @@
+class CompaniesController < ApplicationController
+
+
+def index
+	@companies = Company.all
+	
+end
+
+def show
+	@company = Company.find(params[:id])
+#	@company.records.build
+end
+def new
+    @company = Company.new
+end
+def edit
+   @company = Company.find(params[:id]) 
+end
+def create
+    @company = Company.new(company_params)
+    if @company.save
+        flash[:notice] = "Company was succesfully created"
+        redirect_to company_path(@company)
+    else
+        render 'new'
+    end
+end
+def update
+   @company = Company.find(params[:id])
+   if @company.update(company_params)
+       flash[:notice] = "Company succesfully updated"
+       redirect_to company_path(@company)
+       else
+       render 'edit' 
+   end
+end
+def destroy
+    @company = Company.find(params[:id])
+    @company.destroy
+    flash[:notice] = "Company succesfully deleted"
+           redirect_to companies_path
+end
+private
+
+def company_params
+    params.require(:company).permit(:name)
+end
+
+
+
+
+end
