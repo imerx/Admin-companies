c31cd7e90225220d48b88d5f82a02662595c2a29
Imer D <xidc_xidc@msn.com>
2016-06-07 19:27:14 +0000

    fixed render cart on new modal

--- company_admin/app/controllers/orders_controller.rb
+++ company_admin/app/controllers/orders_controller.rb
@@ -1,52 +1,54 @@
 class OrdersController < ApplicationController
   
   def index
-    @orders = Order.all
-    @cart = current_cart
-    @products = Product.all
-    #@company = Company.find(params[:company_id])
-    
-  end
-
-  def new    
-    
-    @order = Order.new
-    #@company = Company.find(params[:company_id])
-    @products = Product.all   
-    @cart = current_cart
-    
+    @orders = Order.all.order("created_at DESC")
+    #@company = Company.find(params[:company_id])    
   end
   
   def search
     if params[:search]
-      @products = Product.search(params[:search]).order("created_at DESC").limit(2)
- 
+      @products = Product.search(params[:search]).order("created_at DESC").limit(2) 
     else
       redirect_to company_path(@company)
     end   
   end
 
+  def new       
+    @order = Order.new
+    #@company = Company.find(params[:company_id])
+    #@products = Product.all   
+    @cart = current_cart    
+  end
+  
+  
+
   def create
     
     #@order = Order.new(params[:order], order_params )
-    @order = Order.create(order_params)
-    @order.add_line_items_from_cart(current_cart)
- 
-    respond_to do |format|
-      if @order.save
-        Cart.destroy(session[:cart_id])
-        session[:cart_id] = nil
-        format.html { redirect_to(orders_url, :notice => 
-          'Thank you for your order.') }
-        format.xml  { render :xml => @order, :status => :created,
-          :location => @order }
-      else
-        @cart = current_cart
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @order.errors,
-          :status => :unprocessable_entity }
-      end
-    end
+  @order = Order.create(order_params)
+   # @order.add_line_items_from_cart(current_cart)
+   #@post = Post.find(params[:post_id])
+    #@comment = @post.comments.create(comment_params)
+
+    
+    
+
+   respond_to do |format|
+    #if @order.save
+       # Cart.destroy(session[:cart_id])
+       # session[:cart_id] = nil
+       # format.html { redirect_to(orders_url, :notice => 
+         # 'Thank you for your order.') }
+        #format.xml  { render :xml => @order, :status => :created,
+        #  :location => @order }
+        format.js
+       #else
+       # @cart = current_cart
+       # format.html { render :action => "new" }
+       # format.xml  { render :xml => @order.errors,
+         # :status => :unprocessable_entity }
+      
+   end
   end
 
 
